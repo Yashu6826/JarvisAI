74b85afdadb6c9cfe7636e3b7cd2a9b745372576
Yashu6826 <yashrajgupt6826a@gmail.com>
2026-08-05 02:50:38 +0530

    adding cors fix

--- Jarvis Frontend/vite.config.js
+++ Jarvis Frontend/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'node:process'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '..', '')
+  const env = loadEnv(mode, process.cwd(), '')
   return {
-    envDir: '..',
+    envDir: '.',
     plugins: [react()],
     server: {
       proxy: {
